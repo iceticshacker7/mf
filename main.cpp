c54fd979196601d00df6e2b6d5c31eb801b14a28
iceticshacker7 <iceandcold18@gmail.com>
2023-08-24 22:58:25 +0530

    kuch ni bana 2

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 void quicksort(){
-    fefefqfeq
-}
-void mergesort(){
-<<<<<<< HEAD
-    ?WdweDE
-}
-=======
     
 }
->>>>>>> 3d63f0837fb4b20ba0fd231b42e96699562441e1
+void mergesort(){
+    ?WdweDE
+}
